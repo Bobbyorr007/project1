5348103b0bc80b0969a2a4088fe0ca796aa5dd11
Bobmanuel <bobbyorr007@gmail.com>
2023-12-18 01:46:51 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,152 @@
+// // let job = 'front end developer'
+// // console.log(job)
+
+
+// // job = 'back end developer'
+// // console.log(job)
+
+
+
+
+
+// // const course = 'fullstack development'
+// // console.log(course)
+
+
+// // // course = 'data science'
+// // // console.log(course)
+
+
+
+// // const greet = () => {
+// //     return alert ('hello welcome to es6')
+// // }
+// // greet()
+
+
+
+
+
+
+
+
+// const firstname = 'chucks'
+// const work = 'fraudster'
+// const crime = 'cyber fraud'
+// const accomplice = 'Bob'
+// // const sentence = firstname+ ' ' + 'is a '+ ' ' + work + ' ' + 'and his crime is'+ ' ' + crime + ' '+ 'and his accomplice is'+ ' ' + accomplice
+// // console.log(sentence)
+
+// const sentence2 = `${firstname} is a ${work} and his crime is ${crime} and his acomplice is ${accomplice}`
+
+
+
+
+// const stylist = 'Rojjah'
+// const forex = 'christain'
+// const corper = 'chucks'
+// const police = 'daniel'
+// const offender = 'banji'
+
+// const sentence3 = `${stylist} is a friend of mine who met ${forex}, ${corper}, and ${police}. They all drafted out a plan to scam ${offender}`
+// console.log(sentence3)
+
+
+
+// // function age(){
+// //     if (age<18){
+// //         console.log('you are not allowed to vote')
+// //     }
+// //     else{
+// //         console.log('you can vote')
+// //     }
+// // }
+// // age(27)
+
+
+
+
+
+
+
+
+
+const fullStack = [
+    {
+        id : 1,
+        firstName : 'Bobmanuel',
+        job : 'Panel Beater'
+    },
+
+    {
+        id : 2,
+        firstName : 'Rojjah',
+        job : 'Hair Dresser'
+    },
+
+    {
+        id : 3,
+        firstName : 'Bose',
+        job : 'Her Job',
+    },
+
+    {
+        id : 4,
+        firstName : 'Chukwu',
+        job : 'Copper',
+    },
+
+    {
+        id : 5,
+        firstName : 'Nzube',
+        job : 'Drunk',
+    },
+
+    {
+        id : 6,
+        firstName : 'Austine',
+        job : 'G-boy',
+    },
+
+    {
+        id : 7,
+        firstName : 'Christain',
+        job : 'Bouncer',
+    },
+]
+
+
+    let debtor = 'Austine'
+    const findPerson = fullStack.find((person) => person.firstName === debtor)
+    console.log(findPerson)
+
+    let work = 'Drunk'
+    const findDrunk = fullStack.forEach((drinker) => alert(drinker.job))
+    console.log(findDrunk)
+
+
+
+    const product
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
